c0ee59230b7fc9ca1f4a8e6c2d3b199d5ed166df
renzh <zhangren@gmail.com>
2016-11-17 13:18:35 +0800

    Entry link update
    battlemap.php is done!

--- navmenu.php
+++ navmenu.php
@@ -3,7 +3,7 @@
   echo '<hr />';
   echo '<a href="index.php">部落成员</a> &#9775; ';
   echo '<a href="clanwar_start.html">部落战</a> &#9775; ';
-  echo '<a href="battlemap.php?id=0">?更新对阵?</a> &#9775; ';
+  echo '<a href="battlemap.php?id=0">更新对阵</a> &#9775; ';
   echo '<a href="lastbattle.php?id=0&side=1">战况更新</a> &#9775; ';
   echo '<a href="battledata.php?id=0&side=1">部落战结果</a> &#9775; ';
   echo '<a href="battlelist.php?id=0">?部落战统计?</a> &#9775; ';
